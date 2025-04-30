b2915d4383c0ad83db6fe3d7ab2e10c59877927d
Blacklotus88888 <Song.yang.chen89898@gmail.com>
2025-04-30 19:55:43 -0400

    print formatting

--- sandbox/src/main.rs
+++ sandbox/src/main.rs
@@ -1,6 +1,31 @@
 use std::io::{self, Write};
 
 fn main() {
+    // Formatted print
+
+    // Basic formatting
+    println!("{} days", 31);
+
+    // Positional arguments
+    println!("{0}, this is {1}, {1}, this is {0}", "Alice", "Bob");
+    
+    // Named arguments
+    println!("{name} is {age} years old", name = "Alice", age = 30);
+
+    println!("Base 10 number: {}", 42);
+    println!("Base 2 number: {:b}", 42);
+    println!("Base 8 number: {:o}", 42);
+    println!("Base 16 number (hexdecimal): {:x}", 42);
+
+    println!("{number:>5}", number=1); // Right align
+    println!("{number:<5}", number=1); // Left align
+    println!("{number:^5}", number=1); // Center align
+    println!("{number:0>5}", number=1); // Pad with zeros
+
+    println!("{number:0>widht$}", number=1, widht=5); // Pad with custom character
+
+
+
     println!("Hello, world!");
     println!("Welcome to the Rust Terminal App!");
     loop {
@@ -17,15 +42,16 @@ fn main() {
         }
 
         println!("You entered: {}", input);
+        println!("Length: {}", input.len());
 
         passByRef(input);
 
         // passMut(input); // Uncommenting this will cause a compile-time error
         let mut input = String::from(input);
-        passMut(&mut input);
-        // passByValue(input); // Uncommenting this will cause a compile-time error
+        passMut(&mut input); // Mutable reference to the input string
         passByValue(input.clone()); // Cloning to pass by value
-        
+        // passByValue(input); // Uncommenting this will cause a compile-time error
+
 
         // For debugging
         println!("data type:{input:?}");
